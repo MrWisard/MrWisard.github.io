1ff4fc21af8b046b26de8db69b2b8f840e8a9bf5
MrWisard <maxberrystoelzle@icloud.com>
2023-02-16 21:02:29 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById("button1").addEventListener("click", function(){
-    console.log("button1 clicked")
+    console.log("button1 clicked!")
     fetch("https://Test-server.drasiw.repl.co")
             .then(x => {
                  console.log(x);
